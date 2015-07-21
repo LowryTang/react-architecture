bcb0ac9c40250acf22705bb3e856e77b1e854c6a
Lowry <Lowry@lowrydembp.asiapacific.hpqcorp.net>
2015-07-21 17:42:20 +0800

    code commit

--- server.js
+++ server.js
@@ -1,3 +1,10 @@
 var express = require('express');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+
+var React = require('react');
+var App = ;
+
+app.use(function (req, res, next) {
+  var markup = React.renderToString();
+});
